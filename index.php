42cc8895eb1eda4dfac52b7d2c00845092fb9a27
Arrainna <140903846+Arrainna@users.noreply.github.com>
2023-12-20 18:58:29 +0700

    update hw3

---
+++ index.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+        <title><b>Workshop #html-form</b></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="stylist.css">
+    </head>
+       <body>
+        <?php
+        /*<h1>This is PHP <?php echo 'Hello World'; ?>
+        <div>
+            <p>1</p>
+            <?php
+            $var_a = 1;
+            $var_a = "1";
+            print_r($var_a);
+                var_dump($var_a);
+            echo $var_a + 2;  
+            echo $var_a . 2;
+            $arry = array();
+            $arry = [];
+            $arry[] = 1;
+            $arry[] = 2;
+            $arry[0] = 3;
+            print_r($arry);
+            for($i=0;$i < sizeof($arry);$i++){
+                print("$arry[$i]<br>");
+            }
+            { 
+                echo "<h1> my h1 </h1>";
+            ?>
+            <p>2</p>
+            <?php
+            }
+            ?>
+            <p>3</p>
+            
+        </div>*/
+        ?>
+        <?php $multi_x = 10; ?>
+        <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+        <div>
+        <table>
+            <tbody>
+                <?php
+                for($i=1;$i <= 24;$i++){
+                print("<tr>
+                    <td>$i</td>
+                    <td>x</td>
+                    <td>$multi_x</td>
+                    <td>=</td>");
+                $x = $i*$multi_x;
+                print ("<td>$x</td>");
+                print("</tr>");
+                }
+                ?>
+            </tbody>
+        </table>
+        </div>
+    </body>
+    </html>
